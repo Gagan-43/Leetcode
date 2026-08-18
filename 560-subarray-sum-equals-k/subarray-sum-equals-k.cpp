a558496f9b2949251d1ef4962e260d71cad4c556
Gagandeep <gagangusain5@gmail.com>
2026-08-18 23:06:09 +0530

    Time: 38 ms (75.04%) | Memory: 45.4 MB (70.88%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,24 @@
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        int count=0;
+class Solution { 
+public: 
+    int subarraySum(std::vector<int>& nums, int k) { 
+        unordered_map<int, int> mp; 
+        int count = 0; 
+        int pS = 0; 
         
-        for(int i=0;i<nums.size();i++){
-            int sum=0;
-            for(int j=i;j<nums.size();j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+        // Base case: a prefix sum of 0 has been seen once
+        mp[0] = 1; 
+        
+        for (int n : nums) { 
+            pS += n; // Accumulate the running sum
+            
+            // Check if the required complement exists
+            if (mp.find(pS - k) != mp.end()) { 
+                count += mp[pS - k]; 
+            } 
+            
+            // Record the current prefix sum in the map
+            mp[pS]++; 
+        } 
+        return count; 
+    } 
 };
