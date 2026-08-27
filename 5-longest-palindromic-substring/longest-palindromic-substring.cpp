034c2880a9b31a9cae6d5a9848bebfda5d94e611
Gagandeep <gagangusain5@gmail.com>
2026-08-27 21:17:27 +0530

    Time: 3 ms (98.06%) | Memory: 9.4 MB (77.36%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if (s.empty()) return "";
-        int n = s.size();
-        int start = 0, maxLen = 1;
-
-        auto expand = [&](int left, int right) {
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxLen) {
-                    start = left;
-                    maxLen = right - left + 1;
-                }
+        int start = 0;   // longest palindrome ka start index
+        int end = 0;     // longest palindrome ka end index
+        
+        for (int i = 0; i < s.size(); i++) {
+            char currChar = s[i];   // current character
+            
+            int left = i;
+            int right = i;
+            
+            // Step 1: Handle duplicate characters (like "aaa")
+            while (left >= 0 && s[left] == currChar) {
+                left--;
+            }
+            while (right < s.size() && s[right] == currChar) {
+                right++;
+            }
+            
+            // Step 2: Expand around center
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
-        };
-
-        for (int i = 0; i < n; i++) {
-            // Odd length palindrome
-            expand(i, i);
-            // Even length palindrome
-            expand(i, i + 1);
+            
+            // Step 3: Update longest palindrome
+            left++;  // kyunki last decrement extra ho gaya
+            if (end - start < right - left) {
+                start = left;
+                end = right;
+            }
         }
-
-        return s.substr(start, maxLen);
+        
+        // substr(start, length) use karna hota hai
+        return s.substr(start, end - start);
     }
 };
